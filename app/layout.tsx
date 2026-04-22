3f7eb4e9a98298e14574b08aa7ae90de55213db4
santaimany <santaimany@gmail.com>
2026-04-22 21:05:00 +0700

    feat: update layout and globals styles to use Outfit font and define theme variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const outfit = Outfit({
+  variable: "--font-outfit",
   subsets: ["latin"],
 });
 
@@ -23,11 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="flex flex-col min-h-screen">{children}</body>
+    <html lang="en" className={`${outfit.variable} h-full antialiased`}>
+      <body className="font-sans flex flex-col min-h-screen">{children}</body>
     </html>
   );
 }
