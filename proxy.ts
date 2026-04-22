5a882fa4ed11a58c6c1f1d1ba4a45c378152a6d3
santaimany <santaimany@gmail.com>
2026-04-23 00:38:37 +0700

    feat: refactor proxy function to uncomment authentication logic for improved session handling

--- proxy.ts
+++ proxy.ts
@@ -6,20 +6,20 @@ export async function proxy(request: NextRequest) {
   const session = await auth();
   const { pathname } = request.nextUrl;
 
-//   const isAuthPage = pathname === "/login" || pathname === "/register";
-//   const isAdminPage = pathname.startsWith("/admin");
+  const isAuthPage = pathname === "/login" || pathname === "/register";
+  const isAdminPage = pathname.startsWith("/admin");
 
-//   if (!session && !isAuthPage) {
-//     return NextResponse.redirect(new URL("/login", request.url));
-//   }
-//   if (session && isAuthPage) {
-//     return NextResponse.redirect(new URL("/katalog", request.url));
-//   }
-//   if (isAdminPage && session?.user?.role !== "admin") {
-//     return NextResponse.redirect(new URL("/katalog", request.url));
-//   }
+  if (!session && !isAuthPage) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  if (session && isAuthPage) {
+    return NextResponse.redirect(new URL("/katalog", request.url));
+  }
+  if (isAdminPage && session?.user?.role !== "admin") {
+    return NextResponse.redirect(new URL("/katalog", request.url));
+  }
 
-//   return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
